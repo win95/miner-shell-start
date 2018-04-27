fdf28ff886abf513587e8effa07a4f0bcdb7988b
XMR-miner <ducbk55@gmail.com>
2018-04-27 18:59:40 +0700

    Update xmrig-ubuntu-amd.sh

--- xmrig-ubuntu-amd.sh
+++ xmrig-ubuntu-amd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-apt-get update
-apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev tmux screen
-apt-get install -y nvidia-cuda-dev nvidia-cuda-toolkit gcc-5 g++-5
+sudo apt-get update
+sudo apt-get install -y git build-essential cmake libuv1-dev libmicrohttpd-dev tmux screen
+sudo apt-get install -y nvidia-cuda-dev nvidia-cuda-toolkit gcc-5 g++-5
 
 git clone https://github.com/win95/xmr-miner-amd.git
 cd xmr-miner-amd
